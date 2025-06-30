c91adccd9d3d5eef51978e8e9b089cd05a1c2f12
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-06-30 12:04:17 +0530

    Create Code_17.java

---
+++ Code_17.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print outpu
+     to STDOUT. Your class should be named Solution. */
+     Scanner sc=new Scanner(System.in);
+     int n=1;
+     while(sc.hasNext()){
+         String s=sc.nextLine();
+        System.out.println(n+" "+s);
+        n++;
+        
+     }
+    }
+    
+}
